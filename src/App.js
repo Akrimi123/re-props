f56f6be8b0b2f5ac2b278ed883fc0916e4ae7738
Akrimi Rabeh <Rabeh.akrimi1@gmail.com>
2021-05-04 01:42:58 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React,{Component} from 'react';
+import propTypes from "prop-types"
+import {Getsdata} from './Profile/Profile component'
 
-function App() {
+
+class App extends Component{
+render (){
+  function getdata () {
+    alert("Hello From My Page...404 Not Found")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Getsdata FullName=" I'm Akrimi Rabeh." 
+
+        BIO="I can say that applying to create a Web Site has been 
+        the best decision , I have ever made in my life.
+        I love to learn new technologies and flex my creativity to create amazing things."
+        
+        profession="FullStack JS Junior developer." data={getdata}>
+          <div style={{marginTop:30,marginLeft:25,display:'flex'}}><img src="akrimi.jpg"/></div>
+            
+        </Getsdata>
+      
+        
+        
+
+
+          
     </div>
   );
+  }
 }
 
+Getsdata.prototype={
+  FullName:propTypes.string.isRequired ,
+  BIO:propTypes.string.isRequired ,
+  profession:propTypes.string.isRequired ,
+
+};
 export default App;
